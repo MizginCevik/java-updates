92e3e2cefe3e86b01cd4ba57c175ced4284fdb5b
mizgin <mizgincvk@gmail.com>
2022-09-10 18:22:15 -0400

    Stream Task

---
+++ m6-stream/src/main/java/com/cydeo/tasks/CharStreamTask.java
@@ -0,0 +1,19 @@
+package com.cydeo.tasks;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CharStreamTask {
+
+    public static void main(String[] args) {
+
+        //Given a list of words, print the number of characters for each word
+
+        List<String> words = Arrays.asList("JAVA","APPLE","HONDA","DEVELOPER");
+
+        words.stream()
+                .map(str -> str.length())
+                .forEach(System.out::println);
+
+    }
+}
