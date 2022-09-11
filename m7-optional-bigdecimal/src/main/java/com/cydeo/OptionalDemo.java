e5cdda309eaac9ac6436ee9bd1c7b76b63ab38f2
mizgin <mizgincvk@gmail.com>
2022-09-11 13:47:04 -0400

    Optional

---
+++ m7-optional-bigdecimal/src/main/java/com/cydeo/OptionalDemo.java
@@ -0,0 +1,38 @@
+package com.cydeo;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class OptionalDemo {
+
+    public static void main(String[] args) {
+
+        List<Integer> number = Arrays.asList(2,4,2,10,23);
+
+        //empty() - isPresent()
+
+        Optional<String> empty = Optional.empty();
+        System.out.println(empty);
+        System.out.println(empty.isPresent()); //false
+
+        System.out.println(Optional.of(number).isPresent()); //true
+
+        //System.out.println(empty.get()); //no value present --> NoSuchElementException
+
+        // ---> Optional means that it prevents throwing null pointer exception.
+
+        //ifPresent()
+        Optional<Integer> bigNumber = number.stream().filter(x -> x>100).findAny();
+        bigNumber.ifPresent(System.out::println);
+
+        //get()
+        //System.out.println(bigNumber.get()); //no value present --> NoSuchElementException
+
+        //orElse()
+        System.out.println(bigNumber.orElse(5)); //default value
+
+
+
+    }
+}
